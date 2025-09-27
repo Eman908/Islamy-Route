d81b397e7e8b156dc83e528a0f7611701f41ef66
EmanT <tharwateman38@gmail.com>
2025-09-27 20:06:58 +0300

    create splash view and add intro route 1

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:islamy/core/app_colors.dart';
 
 abstract class AppTheme {
-  static ThemeData theme = ThemeData(scaffoldBackgroundColor: AppColors.black);
+  static ThemeData theme = ThemeData(
+    scaffoldBackgroundColor: AppColors.black,
+    fontFamily: 'Janna',
+  );
 }
--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,10 +1,62 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:islamy/core/app_assets.dart';
+import 'package:islamy/core/app_colors.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.bounceIn),
+    );
+    _animationController.forward;
+    // Navigator.of(context).pushReplacementNamed(AppRoutes.introRoute1);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (_, _) {
+          return Transform.scale(
+            scale: _animation.value,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(Assets.assetsImagesLogo),
+                const Spacer(),
+                Image.asset(Assets.assetsImagesRoutegold),
+                const Text(
+                  'Supervised by Mohamed Nabil',
+                  style: TextStyle(color: AppColors.brown, fontSize: 16),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
