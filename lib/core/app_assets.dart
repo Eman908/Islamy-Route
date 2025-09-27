723c69a8793ba5088a404c0f1d23afab169c9a43
EmanT <tharwateman38@gmail.com>
2025-09-27 18:42:18 +0300

    initial the app

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesBearish
+  /// assets/images/bearish.png
+  static const String assetsImagesBearish = "assets/images/bearish.png";
+
+  /// Assets for assetsImagesKabba
+  /// assets/images/kabba.png
+  static const String assetsImagesKabba = "assets/images/kabba.png";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesMosque01
+  /// assets/images/Mosque-01.png
+  static const String assetsImagesMosque01 = "assets/images/Mosque-01.png";
+
+  /// Assets for assetsImagesRadio
+  /// assets/images/radio.png
+  static const String assetsImagesRadio = "assets/images/radio.png";
+
+  /// Assets for assetsImagesReading
+  /// assets/images/reading.png
+  static const String assetsImagesReading = "assets/images/reading.png";
+
+  /// Assets for assetsImagesRoutegold
+  /// assets/images/routegold.png
+  static const String assetsImagesRoutegold = "assets/images/routegold.png";
+
+  /// Assets for assetsImagesWelcome
+  /// assets/images/welcome.png
+  static const String assetsImagesWelcome = "assets/images/welcome.png";
+
+  /// Assets for assetsImagesWelcome2
+  /// assets/images/welcome2.png
+  static const String assetsImagesWelcome2 = "assets/images/welcome2.png";
+}
+
